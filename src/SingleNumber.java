99d9473784b3e2fe6c93d5eae919cdbeea23da15
852788110 <18434362592@163.com>
2019-09-30 21:06:56 +0800

    your old brother in huaihua

---
+++ src/SingleNumber.java
@@ -0,0 +1,5 @@
+public class SingleNumber {
+    public int[] singleNumber(int[] nums) {
+
+    }
+}
--- src/com/BSTIterator.java
+++ src/com/BSTIterator.java
@@ -1,4 +1,39 @@
 package com;
 
 public class BSTIterator {
+    private TreeNode root;
+    public BSTIterator(TreeNode root) {
+        this.root=root;
+    }
+
+    /** @return the next smallest number */
+    /*若有左子树，则遍历左子树
+    * 若无左子树，则最小值为根节点*/
+    public int next() {
+        TreeNode node=root,pre=node;
+        while(node.left!=null){
+            pre=node;
+            node=node.left;
+        }
+        /*int ans=node.val;
+        node=node.right;
+        return ans;*/
+        if (node==pre){
+            int ans=node.val;
+            root=root.right;
+            return ans;
+        }
+        int ans=node.val;
+        if (node.right!=null){
+            pre.left=node.right;
+        }else {
+            pre.left=null;
+        }
+        return ans;
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return root!=null;
+    }
 }
